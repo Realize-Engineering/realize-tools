0eb0aabc0f3b79ed090996fa5af5c3b9c0ecd252
Curtis Mason <curtis@realizefi.com>
2022-06-01 12:26:00 -0400

    added base url

--- plot-historical/main.py
+++ plot-historical/main.py
@@ -7,10 +7,12 @@ load_dotenv()
 
 REALIZE_SECRET_KEY = os.environ.get("REALIZE_SECRET_KEY")
 REALIZE_INSTITUTION_LINK_ID = os.environ.get("REALIZE_INSTITUTION_LINK_ID")
+REALIZE_API_URL = os.environ.get("REALIZE_API_URL")
+
 if __name__ == "__main__":
     print("Begin plotting")
     response = requests.get(
-        f"https://www.realizefi.com/api/institution_links/{REALIZE_INSTITUTION_LINK_ID}/historical_performance?span=month",
+        f"{REALIZE_API_URL}/institution_links/{REALIZE_INSTITUTION_LINK_ID}/historical_performance?span=day",
         headers={"Authorization": f"Bearer {REALIZE_SECRET_KEY}"},
     )
     data = response.json()["data"]
